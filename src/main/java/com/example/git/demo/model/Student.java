b0fda072ace2163c11a57563a29da27e63bc5d23
03unnathi <unnathi246@gmail.com>
2025-02-15 16:58:52 +0530

    creating Student entity

---
+++ src/main/java/com/example/git/demo/model/Student.java
@@ -0,0 +1,33 @@
+package com.example.git.demo.model;
+
+public class Student {
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    private String name;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    private int age;
+}
