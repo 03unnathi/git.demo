17d70c85e62509a02efc0cfec9f372a32f0b0153
03unnathi <unnathi246@gmail.com>
2025-02-15 16:09:14 +0530

    creating user entity

---
+++ src/main/java/com/example/git/demo/model/User.java
@@ -0,0 +1,24 @@
+package com.example.git.demo.model;
+
+public class User {
+    private int id;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+}
